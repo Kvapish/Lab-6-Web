091fc5d8f462d628ad30cfba6ad739cae38c2c53
Kvapish Alexander <89997238+Kvapish@users.noreply.github.com>
2022-06-06 12:53:44 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,62 +1,27 @@
-let $sliderContainer = $('.slider-container');
-let $sliderRow       = $('.slider-container .slider-row');
-let $leftArrow       = $('.slider-container .arrow.left');
-let $rightArrow      = $('.slider-container .arrow.right');
-let $upArrow       = $('.slider-container .arrow.up');
-let $downtArrow      = $('.slider-container .arrow.down');
-let $dots            = $('.slider-container .dots .dot');
+const fullpageApi = $.fn.fullpage;
 
-let slideWidth = $sliderContainer.width();
-let currentSlide = 0;
-let slidesCount = $('.slider-container .slider-row .slide').length;
-
-$sliderRow.css({
-    width: (slideWidth * slidesCount ) + 'px'
-});
-
-function sliderController(slide) {
-    let distance = slide * slideWidth * (-1);
-    $sliderRow.css({
-        left: distance + 'px'
-    });
+function getHash() {
+    return window.location.hash;
 }
-
-$leftArrow.on('click', function () {
-    currentSlide--;
-    if (currentSlide < 0) currentSlide = slidesCount - 1;
-    sliderController(currentSlide);
-});
-$rightArrow.on('click', function () {
-    currentSlide++;
-    if (currentSlide >= slidesCount) currentSlide = 0;
-    sliderController(currentSlide);
-});
-$dots.on('click', function (el) {
-    currentSlide = parseFloat($(this).attr('data-slide')) - 1;
-    sliderController(currentSlide);
-});
-
-
-
-
-
-
-function sliderControllerverctical(slide2) {
-    let distance = slide * slideWidth * (-1);
-    $sliderRow.css({
-        left: distance + 'px'
-    });
+function setHash(value) {
+    window.location.hash = value;
 }
-
-
-
-$upArrow.on('click', function () {
-    currentSlide--;
-    if (currentSlide < 0) currentSlide = slidesCount - 1;
-    sliderController(currentSlide);
+$('.fullpage').fullpage({
+    anchors: ['sport1', 'sport2', 'sport3']
 });
-$downArrow.on('click', function () {
-    currentSlide++;
-    if (currentSlide >= slidesCount) currentSlide = 0;
-    sliderController(currentSlide);
+$('.fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', 'thirdPage'],
+    lockAnchors: true,
+    onLeave: function (origin, destination, direction, trigger) {
+        $('.section[data-anchor="' + origin.anchor + '"]').removeClass('vis');
+        let destinationAnchor = $('[data-index="' + destination.index + '"]').attr('data-anchor');
+        setHash(destinationAnchor);
+    },
+    afterLoad: function (origin, destination, direction, trigger) {
+        $('.section[data-anchor="' + destination.anchor + '"]').addClass('vis');
+    }
+});
+
+$('[data-section]').on('click', function () {
+    fullpageApi.moveTo($(this).attr('data-section'));
 });
